9a806266b1b5bbf36cacec6bd2414d64983b1644
jibey03 <parfaitjeanbaptiste@gmail.com>
2024-03-03 21:39:56 +0100

    Finalisation regroupement de tous les travaux

--- script.js
+++ script.js
@@ -47,20 +47,14 @@ function addProduct() {
 
     var raw = JSON.stringify([
     {
-        "id": 2,
-        "name": "Ticket to Ride2", "img": [
-            "https://cdn1.philibertnet.com/385325-large_default/ticket-to-ride.jpg"
-          ],
-        "description": "Les Aventuriers du Rail vous invite à bord d'une aventure ferroviaire dans laquelle vous collectez des wago pour prendre le contrôle de chemins de fer reliant les différentes villes des États-Unis d’Amérique.",
-        "price":  "100000",
-        "ref": "DOW7201",
-        "age":  "à partir de 108 ans",
-        "nbPlayers": "2 à 5 joueur(s)",
-        "timing": "30mn à 1h",
-        "video": "https://www.youtube.com/embed/4JhFhyvGdik"
-      }
+        "id": id,
+        "name": name,
+        "description": description,
+        "price": price,
+        "ref": ref,
+        "age": age,
+    }
     ]);
-
 
     var requestOptions = {
     method: 'POST',
